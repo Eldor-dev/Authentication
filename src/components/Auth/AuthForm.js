b7bfed1378a301bf5d9a3cbf360e7a82160e6736
Eldor Azimov <eldorazimov37@gmail.com>
2022-12-28 15:27:14 +0900

    Adding User Login

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -20,36 +20,43 @@ const AuthForm = () => {
 
     // Add validation
     setIsLoading(true);
+    let url;
     if (isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBduvuVp96srxmggpQCFUnps42k8KtVsHM";
     } else {
-      fetch(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBduvuVp96srxmggpQCFUnps42k8KtVsHM",
-        {
-          method: "POST",
-          body: JSON.stringify({
-            email: enteredEmail,
-            password: enterdPassword,
-            returnSecureToken: true,
-          }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      ).then((res) => {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBduvuVp96srxmggpQCFUnps42k8KtVsHM";
+    }
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enterdPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
         setIsLoading(false);
         if (res.ok) {
-          //...
+          return res.json();
         } else {
           return res.json().then((data) => {
             let errorMessage = "Authentication failed!";
             if (data && data.error && data.error.message) {
               errorMessage = data.error.message;
             }
-            alert(errorMessage);
+            throw new Error(errorMessage);
           });
         }
-      });
-    }
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => alert(err.message));
   };
 
   return (
